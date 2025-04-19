67934cdb034ac02f7d8ab0d231f3de1d1135375e
LucF120 <lucjferrara@gmail.com>
2025-04-19 13:03:45 -0400

    installed axios, called it in a temporary function that makes a get request to the api

--- wordle_client/src/client.ts
+++ wordle_client/src/client.ts
@@ -1,3 +1,8 @@
 import axios from "axios";
 
 const API = import.meta.env.VITE_API_URL || "https://localhost:3000";
+
+export const temp = async () => {
+	const { data } = await axios.get(`${API}/`);
+	return data;
+}

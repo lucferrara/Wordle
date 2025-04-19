39bb5336f7e7f30a89a1423ad9abf3edcec26ddc
LucF120 <lucjferrara@gmail.com>
2025-04-19 13:43:54 -0400

    Added bootstrap to app

---
+++ wordle_client/src/Wordle/Start.tsx
@@ -0,0 +1,11 @@
+export default function Start() {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-6">
+                    <button className="btn btn-success">Start</button>
+                </div>
+            </div>
+        </div>
+    )
+}

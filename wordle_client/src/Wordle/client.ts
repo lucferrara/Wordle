553f9e07ffb09551d0d015925ded802af4ab2963
LucF120 <lucjferrara@gmail.com>
2025-04-19 20:06:54 -0400

    created function in client that sends request to API with a guess

--- wordle_client/src/Wordle/client.ts
+++ wordle_client/src/Wordle/client.ts
@@ -6,3 +6,8 @@ export const startGame = async () => {
 	const { data } = await axios.get(`${API}/startgame`);
 	return data;
 }
+
+export const sendGuess = async (gameId: Number, guess: string) => {
+	const { data } = await axios.post(`${API}/sendguess/${gameId}/${guess}`);
+	return data;
+}

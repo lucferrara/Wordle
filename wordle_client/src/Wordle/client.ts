c7eaa2407b654aefdc1e401a38f8f440174ec56a
LucF120 <lucjferrara@gmail.com>
2025-09-22 23:32:01 -0400

    hardcode api

--- wordle_client/src/Wordle/client.ts
+++ wordle_client/src/Wordle/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API = "https://lucferrara.com";
 
 export const startGame = async () => {
 	const { data } = await axios.get(`${API}/startgame`);
